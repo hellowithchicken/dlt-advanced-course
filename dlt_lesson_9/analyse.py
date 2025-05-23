15c18b5ac43ad602a9893435dd09987106b74a54
kriksix <kriksix@abbott.com>
2025-05-23 09:30:10 +0200

    clean up

--- dlt_lesson_9/analyse.py
+++ dlt_lesson_9/analyse.py
@@ -1,19 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     custom_cell_magics: kql
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.11.2
-#   kernelspec:
-#     display_name: aida_aws_3_9
-#     language: python
-#     name: python3
-# ---
-
 # %%
 
 ############################################################
